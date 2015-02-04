7760637fc8d51bfed9381d7f7b006cb00d9c2893
Nathan Geslin <ngeslin@dhcp-137.bravo>
2015-02-04 09:00:04 -0600

    Initialize

---
+++ src/assets/scripts/App.js
@@ -0,0 +1,31 @@
+define(function(require, exports, module) { // jshint ignore:line
+    'use strict';
+    
+    require('nerdery-request-animation-frame');
+    var Modernizr = require('modernizr');
+
+    /**
+     * Initial application setup. Runs once upon every page load.
+     *
+     * @class App
+     * @constructor
+     */
+    var App = function() {
+        this.init();
+    };
+
+    var proto = App.prototype;
+
+    /**
+     * Initializes the application and kicks off loading of prerequisites.
+     *
+     * @method init
+     * @private
+     */
+    proto.init = function() {
+        // Create your views here
+    };
+
+    return App;
+
+});
---
+++ tools/tasks/buildMarkup.js
@@ -0,0 +1,76 @@
+/*jshint node:true, laxbreak:true */
+'use strict';
+
+module.exports = function(grunt) {
+    var shouldMinify = !grunt.option('dev');
+
+    grunt.config.merge({
+        hbt: {
+            buildMarkup: {
+                options: {
+                    data: {
+                        pkg: '<%= pkg %>',
+                        env: '<%= env %>'
+                    },
+                    helpers: [
+                        '<%= env.DIR_NPM %>/handlebars-layouts/index.js'
+                    ],
+                    partials: [
+                        '<%= env.DIR_SRC %>/templates/**/*.hbs'
+                    ]
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= env.DIR_SRC %>',
+                    dest: '<%= env.DIR_TMP %>',
+                    ext: '.html',
+                    src: [
+                        '**/*.hbs',
+                        '!templates/**',
+                        '!assets/vendor/**'
+                    ]
+                }]
+            }
+        },
+
+        prettify: {
+            buildMarkup: {
+                options: {
+                    indent: 4,
+                    wrap_line_length: 999999, // jshint ignore:line
+                    indent_inner_html: false, // jshint ignore:line
+                    unformatted: [
+                        'a', 'b', 'code', 'i', 'p',
+                        'pre', 'small', 'span',
+                        'sub', 'sup', 'u', 'textarea'
+                    ]
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= env.DIR_TMP %>',
+                    dest: '<%= env.DIR_DEST %>',
+                    src: ['**/*.html']
+                }]
+            }
+        },
+
+        // Replaces script and style tag references with a reference to a
+        // single optimized output file.
+        usemin: {
+            html: ['<%= env.DIR_DEST %>/**/*.html']
+        }
+    });
+
+    grunt.registerTask('buildMarkup',
+        shouldMinify
+            ? [
+                'hbt:buildMarkup',
+                'prettify:buildMarkup',
+                'usemin'
+            ]
+            : [
+                'hbt:buildMarkup',
+                'prettify:buildMarkup'
+            ]
+    );
+};
